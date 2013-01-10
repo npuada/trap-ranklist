4de573c0d418852070b5c841a41a69e99f405cf7
Nix <nixpuada@gmail.com>
2013-01-11 00:56:30 +0800

    Added Procfile and requirements.txt

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import ranklist
 from flask import Flask, render_template
 
@@ -10,4 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
